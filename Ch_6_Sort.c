1f0d84d9935d45c42375f06fff54edb0e34dd483
dksj125 <dksj125@gmail.com>
2020-09-20 02:11:15 +0900

    qsort

--- Ch_6_Sort.c
+++ Ch_6_Sort.c
@@ -316,7 +316,7 @@ int main() {
 */
 
 // Example Shell Algorithm
-
+/*
 #include<stdio.h>
 void shell(int a[], int n) {
 	int i, j, h;
@@ -339,6 +339,182 @@ int main() {
 	for (int i = 0; i < sizeof(a) / sizeof(int); i++) {
 		printf("%d\n", a[i]);
 	}
+
+	return 0;
+}
+*/
+
+
+/*Quick Sort*/
+
+// practice 6-8 피벗기준으로 나누기
+/*
+#include<stdio.h>
+#include<stdlib.h>
+
+#define swap(type, x, y) do{type t = x; x = y; y = t;} while(0)
+
+void partition(int a[], int n) {
+	int i;
+	int pl = 0;
+	int pr = n - 1;
+	int x = a[n / 2];
+	do {
+		while (a[pl] < x) pl++;
+		while (a[pr] > x) pr--;
+		if (pl <= pr) {
+			swap(int, a[pl], a[pr]);
+			pl++;
+			pr--;
+		}
+	} while (pl <= pr);
+	printf("피벗의 값은 %d입니다.\n", x);
+	printf("피벗 이하의 그룹\n");
+	for (i = 0; i <= pl - 1; i++) {
+		printf("%d ", a[i]);
+	}
+	putchar('\n');
+	if (pl > pr + 1) {
+		printf("피벗과 일치하는 그룹\n");
+		for (i = pr + 1; i <= pl - 1; i++)
+			printf("%d ", a[i]);
+		putchar('\n');
+	}
+	printf("피벗 이상의 그룹\n");
+	for (i = pr + 1; i < n; i++)
+		printf("%d ", a[i]);
+	putchar('\n');
+}
+
+
+int main() {
+
+	int i, nx;
+
+	int* x;
+	puts("배열을 나눕니다.");
+	printf("요소 개수 : ");
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int));
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+	partition(x, nx);
+	free(x);
+
+	return 0;
+}
+*/
+
+
+//practice 6-9
+/*퀵 정렬 함수*/
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#define swap(type, x, y) do{type t = x ; x= y; y = t;} while(0)
+
+
+
+void quick(int a[], int left, int right) {
+	int pl = left;
+	int pr = right;
+	int x = a[(pl + pr) / 2];
+	do {
+		while (a[pl] < x) pl++;
+		while (a[pr] > x) pr--;
+		if (pl <= pr) {
+			swap(int, a[pl], a[pr]);
+			pl++;
+			pr--;
+		}
+	} while (pl <= pr);
+	if (left < pr) quick(a, left, pr);
+	if (pl < right) quick(a, pl, right);
+}
+
+int main() {
+	int i, nx;
+	int* x;
+	puts("퀵 정렬");
+	printf("요소 개수 : ");
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int));
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+	quick(x, 0, nx - 1);
+	puts("오름차순으로 정렬했습니다.");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+	free(x);
+
+	return 0;
+}
+*/
+
+//practice 6-12
+/*qsort함수 상요하기*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct {
+	char name[10];
+	int height;
+	int weight;
+}Person;
+
+
+int npcmp(const Person* x, const Person* y) {
+	return strcmp(x->name, y->name);
+}
+
+int hpcmp(const Person* x, const Person* y) {
+	return x->height < y->height ? -1 :
+		x->height > y->height ? 1:0;
+}
+
+int wpcmp(const Person* x, const Person* y) {
+	return x->weight < y->weight ? 1 :
+		x->weight > y->weight ? -1 : 0;
+}
+
+void print_person(const Person x[], int no) {
+	int i;
+	for (i = 0; i < no; i++) {
+		printf("%-10s %dcm %dkg\n", x[i].name, x[i].height, x[i].weight);
+	}
+}
+
+int main() {
+	Person x[] = {
+		{"sunmi", 170, 52},
+		{"yoobun", 180, 70},
+		{"soohee", 172, 63},
+		{"jina", 165, 50},
+	};
+
+	int nx = sizeof(x) / sizeof(x[0]);
+
+	puts("정렬 전");
+	print_person(x, nx);
+
+
+	qsort(x, nx, sizeof(Person), (int(*)(const void*, const void*))npcmp);
+	puts("\n이름 오름차순으로 정렬 후");
+	print_person(x, nx);
+
+	qsort(x, nx, sizeof(Person), (int(*)(const void*, const void*))hpcmp);
+	puts("\n키 오름차순으로 정렬 후");
+	print_person(x, nx);
+
+	qsort(x, nx, sizeof(Person), (int(*)(const void*, const void*))wpcmp);
+	puts("\n몸무게 오름차순으로 정렬 후");
+	print_person(x, nx);
 
 	return 0;
 }
