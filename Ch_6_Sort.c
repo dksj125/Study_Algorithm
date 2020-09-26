bffa16c32b96b507fa9e9eb97945e335bf1a6934
dksj125 <dksj125@gmail.com>
2020-09-27 02:06:25 +0900

    practice how to make merge sort

--- Ch_6_Sort.c
+++ Ch_6_Sort.c
@@ -456,8 +456,8 @@ int main() {
 */
 
 //practice 6-12
-/*qsort함수 상요하기*/
-
+/*qsort함수 사용하기*/
+/*
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -515,6 +515,121 @@ int main() {
 	qsort(x, nx, sizeof(Person), (int(*)(const void*, const void*))wpcmp);
 	puts("\n몸무게 오름차순으로 정렬 후");
 	print_person(x, nx);
+
+	return 0;
+}
+*/
+
+
+//practice 6-14
+/*병합 정렬*/
+/*
+#include<stdio.h>
+
+void merge(const int a[],int na,const int b[], int nb, int c[]) {
+	int pa = 0;
+	int pb = 0;
+	int pc = 0;
+	while (pa < na && pb < nb)
+		c[pc++] = (a[pa] <= b[pb]) ? a[pa++] : b[pb++];
+	while (pa < na)
+		c[pc++] = a[pa++];
+	while (pb < na)
+		c[pc++] = b[pb++];
+}
+
+int main() {
+
+	int i, na, nb;
+	int* a, * b, * c;
+	printf("a의 요소 개수 : "); scanf("%d", &na);
+	printf("b의 요소 개수 : "); scanf("%d", &nb);
+	a = calloc(na, sizeof(int));
+	b = calloc(nb, sizeof(int));
+	c = calloc(na + nb, sizeof(int));
+
+	printf("a[0]: ");
+	scanf("%d", &a[0]);
+	for (i = 1; i < na; i++) {
+		do {
+			printf("a[%d] : ", i);
+			scanf("%d", &a[i]);
+		} while (a[i] < a[i - 1]);
+	}
+	printf("b[0] : "); scanf("%d", &b[0]);
+
+	for (i = 1; i < nb; i++) {
+		do {
+			printf("b[%d] : ", i);
+			scanf("%d", &b[i]);
+		} while (b[i] < b[i - 1]);
+	}
+
+	merge(a, na,b, nb, c);
+	puts("배열 a와 b를 병합하여 배열 c에 저장했습니다.");
+	for (i = 0; i < na + nb; i++) {
+		printf("c[%d] = %d\n", i, c[i]);
+	}
+
+	free(a);
+	free(b);
+	free(c);
+
+	return 0;
+}
+*/
+
+//practice 6-15
+
+#include<stdio.h>
+#include<stdlib.h>
+
+static int* buff;
+
+static void __mergesort(int a[], int left, int right) {
+	if (left < right) {
+		int center = (left + right) / 2;
+		int p = 0;
+		int i;
+		int j = 0;
+		int k = left;
+		__mergesort(a, left, center);
+		__mergesort(a, center + 1, right);
+		for (i = left; i <= center; i++)
+			buff[p++] = a[i];
+		while (i <= right && j < p)
+			a[k++] = (buff[j] <= a[i]) ? buff[j++] : a[i++];
+		while (j < p)
+			a[k++] = buff[j++];
+	}
+}
+
+int mergesort(int a[], int n) {
+	if ((buff = calloc(n, sizeof(int))) == NULL)
+		return -1;
+	__mergesort(a, 0, n - 1);
+	free(buff);
+	return 0;
+}
+
+int main() {
+	int i, nx;
+	int* x;
+	puts("병합 정렬");
+	printf("요소 개수 : "); scanf("%d", &nx);
+	x = calloc(nx, sizeof(int));
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d]: ", i);
+		scanf("%d", &x[i]);
+	}
+	mergesort(x, nx);
+
+	puts("오름차순으로 정렬했습니다.");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+
+	free(x);
 
 	return 0;
 }
